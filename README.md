89ec2587af4a41b55d82d338abcb8ef9a1d99fac
AlMoqanna <micorreodelauba@gmail.com>
2019-08-20 20:16:48 -0300

    Adding second exercise

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # algor2Practices
 Exercises from algoritmus 2
+Note that executables files must be end in .e 
+e.g: myFile.e
+This is just for use the .gitignore file in an easy way 
---
+++ unit1/greet.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+  string name;
+  cout << "Please give me votre nom: "<< endl;
+  cin >> name;
+  cout << "Bonjour " << name << ". How are you?" << endl;
+  return 0;
+}
